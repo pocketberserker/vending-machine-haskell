04017fd26c240a2d0f4d83501b87dfade8817081
pocketberserker <pocketberserker@gmail.com>
2012-07-23 19:34:54 +0900

    [from now] 2012/07/23 19:34:54

--- src/MoneyStack.hs
+++ src/MoneyStack.hs
@@ -25,6 +25,8 @@ init =
 
 >>> total MoneyStack {ten = 1, fifty = 0, hundred = 0, fiveHundred = 0, thousand = 0}
 10
+>>> total MoneyStack {ten = 1, fifty = 1, hundred = 1, fiveHundred = 1, thousand = 1}
+1660
 -}
 total = sum . sequence
   [(*10) . ten, (*50) . fifty, (*100) . hundred, (*500) . fiveHundred, (*1000) . thousand]
