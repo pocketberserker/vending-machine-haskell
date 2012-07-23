7f64df27c00504b538b0a1e1cb14b8f554f8eca9
pocketberserker <pocketberserker@gmail.com>
2012-07-23 19:39:43 +0900

    投入したお金を払い戻せるようにした

--- src/VendingMachine.hs
+++ src/VendingMachine.hs
@@ -27,3 +27,16 @@ insert 100 = state $ \s -> (Nothing, s { hundred = hundred s + 1})
 insert 500 = state $ \s -> (Nothing, s { fiveHundred = fiveHundred s + 1})
 insert 1000 = state $ \s -> (Nothing, s { thousand = thousand s + 1})
 insert other = state $ \s -> (Just other, s)
+
+{-| function 'payback'
+
+>>> runState payback $ execState (insert 10) MoneyStack.init
+([10],MoneyStack {ten = 0, fifty = 0, hundred = 0, fiveHundred = 0, thousand = 0})
+>>> runState payback $ execState (insert 10 >> insert 1000) MoneyStack.init
+([10,1000],MoneyStack {ten = 0, fifty = 0, hundred = 0, fiveHundred = 0, thousand = 0})
+-}
+payback :: State MoneyStack [Int]
+payback = state $ \s -> (calc s, MoneyStack.init)
+  where convert s (f, m) = take (f s) $ repeat m
+        calc s = concat $ map (convert s)
+	  [(ten, 10), (fifty, 50), (hundred, 100), (fiveHundred, 500), (thousand, 1000)]
