cbca24d8e6af221c403ebcf5099da0bd62145c2b
pocketberserker <pocketberserker@gmail.com>
2012-07-23 19:10:14 +0900

    [from now] 2012/07/23 19:10:14

--- src/VendingMachine.hs
+++ src/VendingMachine.hs
@@ -1,8 +1,11 @@
 module VendingMachine where
 
+import Control.Monad.State
+import MoneyStack
+
 {-| function 'insert'
 
 >>> runState (insert 10) MoneyStack.init
 (Nothing,MoneyStack {ten = 1, fifty = 0, hundred = 0, fiveHundred = 0, thousand = 0})
 -}
-insert = undefined
+insert 10 = state $ \s -> (Nothing, s { ten = 1})
