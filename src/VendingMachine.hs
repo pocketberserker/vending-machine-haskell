f211837170d59445ba4d53093fe83ca61c82e4c9
pocketberserker <pocketberserker@gmail.com>
2012-07-23 19:12:25 +0900

    [from now] 2012/07/23 19:12:25

--- src/VendingMachine.hs
+++ src/VendingMachine.hs
@@ -7,5 +7,7 @@ import MoneyStack
 
 >>> runState (insert 10) MoneyStack.init
 (Nothing,MoneyStack {ten = 1, fifty = 0, hundred = 0, fiveHundred = 0, thousand = 0})
+>>> runState (insert 10 >> insert 10) MoneyStack.init
+(Nothing,MoneyStack {ten = 2, fifty = 0, hundred = 0, fiveHundred = 0, thousand = 0})
 -}
 insert 10 = state $ \s -> (Nothing, s { ten = 1})
