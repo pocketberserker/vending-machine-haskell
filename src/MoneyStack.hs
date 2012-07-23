8d5b2b8ca35abd2460f7453ca62fc9bbbfe5f3cb
pocketberserker <pocketberserker@gmail.com>
2012-07-23 18:48:28 +0900

    投入金額状態を初期化できるようにした

--- src/MoneyStack.hs
+++ src/MoneyStack.hs
@@ -6,3 +6,15 @@ data MoneyStack =
 	     , hundred :: Int
 	     , fiveHundred :: Int
 	     , thousand :: Int} deriving (Show)
+
+{-| function 'init'
+
+>>> MoneyStack.init
+MoneyStack {ten = 0, fifty = 0, hundred = 0, fiveHundred = 0, thousand = 0}
+-}
+init =
+  MoneyStack { ten = 0
+             , fifty = 0
+	     , hundred = 0
+	     , fiveHundred = 0
+	     , thousand = 0}
