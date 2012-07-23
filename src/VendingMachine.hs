e7ad484e2768ae2f66fc726518acf3dc05072648
pocketberserker <pocketberserker@gmail.com>
2012-07-23 19:07:01 +0900

    [from now] 2012/07/23 19:07:01

--- src/VendingMachine.hs
+++ src/VendingMachine.hs
@@ -1,2 +1,8 @@
 module VendingMachine where
 
+{-| function 'insert'
+
+>>> runState (insert 10) MoneyStack.init
+(Nothing,MoneyStack {ten = 1, fifty = 0, hundred = 0, fiveHundred = 0, thousand = 0})
+-}
+insert = undefined
