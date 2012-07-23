8698a5c6d8b9e12eae047eaabd12ee9330e348aa
pocketberserker <pocketberserker@gmail.com>
2012-07-23 19:24:34 +0900

    投入金額の合計を算出できるようにした

--- src/MoneyStack.hs
+++ src/MoneyStack.hs
@@ -1,5 +1,7 @@
 module MoneyStack where
 
+import Control.Monad.List ()
+
 data MoneyStack =
   MoneyStack { ten :: Int
              , fifty :: Int
@@ -18,3 +20,13 @@ init =
 	     , hundred = 0
 	     , fiveHundred = 0
 	     , thousand = 0}
+
+{-| function 'total'
+
+>>> total MoneyStack {ten = 1, fifty = 0, hundred = 0, fiveHundred = 0, thousand = 0}
+10
+>>> total MoneyStack {ten = 1, fifty = 1, hundred = 1, fiveHundred = 1, thousand = 1}
+1660
+-}
+total = sum . sequence
+  [(*10) . ten, (*50) . fifty, (*100) . hundred, (*500) . fiveHundred, (*1000) . thousand]
